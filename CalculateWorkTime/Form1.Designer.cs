f5779e1ec1c8ef8fccbc74014eed23812459df44
Sadullah <sadullah.dogan@yandex.com>
2023-03-29 23:02:58 +0300

    Add project files.

---
+++ CalculateWorkTime/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CalculateWorkTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputHours = new System.Windows.Forms.TextBox();
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInputHours
+            // 
+            this.txtInputHours.Location = new System.Drawing.Point(30, 60);
+            this.txtInputHours.Multiline = true;
+            this.txtInputHours.Name = "txtInputHours";
+            this.txtInputHours.Size = new System.Drawing.Size(230, 336);
+            this.txtInputHours.TabIndex = 0;
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.AutoSize = true;
+            this.labelInformation.Location = new System.Drawing.Point(30, 20);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(0, 15);
+            this.labelInformation.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(30, 402);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(230, 36);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Hesapla";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Toplam Çalışma Süresi : ";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(448, 60);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.labelInformation);
+            this.Controls.Add(this.txtInputHours);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputHours;
+        private System.Windows.Forms.Label labelInformation;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
---
+++ CalculateWorkTime/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateWorkTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelInformation.Text = "Günlük Çalışma saat aralığı format bilgileri (09:30-12:30 13:30-17:30) gibi farklı aralıklar arasında boşluk bırakın";
+            /*
+             8:20-12:45 13:30-17:17
+             7:57-12:20 13:30-17:07
+             8:18-5:12
+             8:12-5:10
+             8:04-5:00
+             8:10-12:16 14:00-17:10
+             8:00-12:00 13:00-17:15 
+             8:30-16:30
+             8:00-17:15
+             */
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var input = txtInputHours.Text;
+            var lines = input.Split("\r\n");
+            string errorText = "";
+            var result = CalculateRangeFromInput(lines, ref errorText);
+            lblResult.Text = result + $" Dakika \r\n {result / 60.0} Saat\r\n{errorText}";
+        }
+        private double CalculateRangeFromInput(string[] lines, ref string errorText)
+        {
+            double minute = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                var ranges = line.Trim().Split(' ');
+                for (int j = 0; j < ranges.Length; j++)
+                {
+                    var range = ranges[j];
+                    string error = "";
+                    minute += CalculateMinuteFromTimeRange(range, i, j, ref error);
+                    errorText += error;
+                }
+            }
+            return minute;
+        }
+        private double CalculateMinuteFromTimeRange(string range, int i, int j, ref string hasError)
+        {
+            double minute = 0;
+            try
+            {
+                //09:30-12:30 input format
+
+                var dates = range.Split('-');
+                if (dates.Length != 2)
+                    throw new InvalidEnumArgumentException($"{i + 1} Satırda {j + 1} aralıkta hatalı aralık. \r\n");
+                var startTimeArray = dates[0].Split(":");
+                var endTimeArray = dates[1].Split(":");
+                if (startTimeArray.Length != 2 || endTimeArray.Length != 2)
+                    throw new InvalidEnumArgumentException($"{i + 1} Satırda {j + 1} aralıkta hatalı data. \r\n");
+                var hour1 = int.Parse(startTimeArray[0]);
+                var minute1 = int.Parse(startTimeArray[1]);
+                var hour2 = int.Parse(endTimeArray[0]);
+                var minute2 = int.Parse(endTimeArray[1]);
+                var startTime = new DateTime(2000, 10, 1, hour1, minute1, 0);
+                var endTime = new DateTime(2000, 10, 1, hour2, minute2, 0);
+                if (startTime > endTime)
+                    throw new InvalidEnumArgumentException($"{i + 1} Satırda {j + 1} aralıkta başlangıç tarihi bitiş tarihinden büyük. \r\n");
+                TimeSpan span = endTime.Subtract(startTime);
+                minute = span.TotalMinutes;
+
+                return minute;
+            }
+            catch (InvalidEnumArgumentException e)
+            {
+                hasError += e.Message;
+                return minute;
+            }
+            catch (Exception e)
+            {
+
+                hasError += $"{i + 1} Satırda {j + 1} aralıkta hatalı input. \r\n";
+                return minute;
+            }
+
+        }
+    }
+}
